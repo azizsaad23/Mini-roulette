5aa1ff5089033f6f548ad37e9f5b4eaadf56fc1b
azizsaad23 <azizsaad1998@gmail.com>
2021-01-12 00:31:32 +0100

    Mini Roulette

--- roulette.js
+++ roulette.js
@@ -1,14 +1,13 @@
-function aziz () {
-	var wheel = document.querySelector('.wheel');
-	var startbutton = document.querySelector(".button");
-	var deg = 0
-	
 
-	startbutton.addEventListener("click",() => {
-		startbutton.style.pointerEvents= "none";
-		deg= Math.floor(5000 + Math.random() * 5000);
-		wheel.style.transition = "all 10s ease-out";
-		wheel.style.transform = `rotate(${deg}deg`;
-		wheel.classList.add("blur")
-	})
+alert("Welcome To Mini Roulette")
+var currentLength = -4.86
+
+$(".click").click(function() {
+	currentLength+= 1080 + (getRandomInt(0, 37)* 9.729)
+
+	
+	$(".wheel #wl").css("transform",'rotate(-' +currentLength + 'deg)')
+})
+function getRandomInt(min,max){
+	return Math.floor(Math.random()* (max - min +1))+min
 }
