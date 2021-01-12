114033f24e519338176190e45065be86923d1c40
azizsaad23 <azizsaad1998@gmail.com>
2021-01-12 02:18:11 +0100

    Mini Roulette

--- roulette.js
+++ roulette.js
@@ -1,15 +1,17 @@
 
-alert("Welcome To Mini Roulette")
+ alert("Welcome To Mini Roulette");
 var currentLength = -4.86
 
-$(".click").click(function() {
+$(".click").click(function click() {
 	currentLength+= 1080 + (getRandomInt(0, 37)* 9.729)
 
 	
 	$(".wheel #wl").css("transform",'rotate(-' +currentLength + 'deg)')
+
 })
 function getRandomInt(min,max){
 	return Math.floor(Math.random()* (max - min +1))+min
-
+      
 }
+
 
